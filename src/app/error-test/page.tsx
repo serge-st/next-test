aa5a7c80f6a9f326c1643c247e49f6426ae0aaae
Serge Stecenko <sergejs.stecenko@gmail.com>
2023-10-05 18:16:59 +0300

    feat: implement error fallback ui for server component

--- src/app/error-test/page.tsx
+++ src/app/error-test/page.tsx
@@ -1,21 +1,12 @@
-'use client'
-
 export default function ErrorPage() {
-    const handleClick = () => {
-        throw new Error('now were\'re talking')
+    if (Math.random() > 0.5) {
+      throw new Error('oooops')
     }
 
     return(
       <div className="flex flex-col w-screen h-full justify-center items-center">
         <h1>Welcome to error page</h1>
-        <p>No error yet, but you can</p>
-        <button 
-            className="border border-solid rounded px-6 py-3"
-            type="button"
-            onClick={handleClick}
-        >
-            Throw an <span className="text-red-500 font-bold">error</span>
-        </button>
+        <p>No error yet, but you can get one 😵</p>
       </div>
     )
 }
