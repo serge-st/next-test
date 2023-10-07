e7d16805e32ab8d4ab20d16332f37cff8003ec52
Serge Stecenko <sergejs.stecenko@gmail.com>
2023-10-07 14:46:37 +0300

    feat: add loading ui

---
+++ src/app/client-component-with-server-component/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div className='flex flex-col justify-center items-center h-screen'>
+            Loading...
+        </div>
+    );
+};
+
+export default loading;
